e36a007017b04253660faf9109190bbd9f1ebb43
cc <hnlkxyz@163.com>
2019-10-19 16:17:27 +0800

    整合

--- manage/src/main/java/com/lanou/manage/service/UsersService.java
+++ manage/src/main/java/com/lanou/manage/service/UsersService.java
@@ -2,13 +2,7 @@ package com.lanou.manage.service;
 
 import com.lanou.manage.bean.Users;
 
-import java.util.List;
-
 public interface UsersService {
-    public List<Users> userList();
-    public int findUser(Integer account);
-    public int addUser(Users users);
-    public int updateUser(Users users);
-    //删除用户
-    public int deleteUser(Integer account);
+    //登录
+    Users isLogin(Users users);
 }
--- manage/src/main/java/com/lanou/manage/service/impl/UsersServiceImpl.java
+++ manage/src/main/java/com/lanou/manage/service/impl/UsersServiceImpl.java
@@ -6,36 +6,13 @@ import com.lanou.manage.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UsersServiceImpl implements UsersService {
     @Autowired
     private UsersMapper usersMapper;
 
-
     @Override
-    public List<Users> userList() {
-        return usersMapper.userList();
-    }
-
-    @Override
-    public int findUser(Integer account) {
-        return usersMapper.findUser(account);
-    }
-
-    @Override
-    public int addUser(Users users) {
-        return usersMapper.addUser(users);
-    }
-
-    @Override
-    public int updateUser(Users users) {
-        return usersMapper.updateUser(users);
-    }
-
-    @Override
-    public int deleteUser(Integer account) {
-        return usersMapper.deleteUser(account);
+    public Users isLogin(Users users) {
+        return usersMapper.isLogin(users);
     }
 }
